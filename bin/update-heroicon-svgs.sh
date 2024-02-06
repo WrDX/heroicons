2c02cccc7b59e07011c267d829b6b3a0b4c15327
Wouter <wouter@wrdx.nl>
2024-02-06 21:55:16 +0100

    Use tmp dir for sparce checkout. Moved svg files

--- bin/update-heroicon-svgs.sh
+++ bin/update-heroicon-svgs.sh
@@ -1,41 +1,64 @@
 #!/usr/bin/env bash
 
-# Make sure we're in the right directory
+echo "Checking newest version of tailwindlabs/heroicons..."
+
+# Move to basedir
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BASEDIR="${SCRIPT_DIR}/../"
-cd "${BASEDIR}" || exit
+BASEDIR="${SCRIPT_DIR}/.."
+cd "${BASEDIR}/" || exit
 
-# Reset resources directory
-echo "Resetting resources directory"
-
-if [ -d "${BASEDIR}/resources/heroicons" ]; then
-  rm -Rf "${BASEDIR}/resources/heroicons"
+# Read current version
+currentVersion="0"
+if [ -f "${BASEDIR}/resources/heroicons/.version" ]; then
+  currentVersion=$(cat "${BASEDIR}/resources/heroicons/.version")
 fi
-mkdir -p "${BASEDIR}/resources/heroicons"
 
-# Pull heroicons/optimized directory
-echo "Fetching heroicons git repo"
+# Create a tmp dir to do stuff
+if [ -d "${BASEDIR}/.tmp" ]; then
+  rm -Rf "${BASEDIR}/.tmp"
+fi
+mkdir -p "${BASEDIR}/.tmp"
+cd "${BASEDIR}/.tmp/" || exit
 
-cd "${BASEDIR}/resources/heroicons" || exit
+# Fetch tailwindlabs/heroicons to check latest version
 
 git init -b main -q
 git remote add -f origin https://github.com/tailwindlabs/heroicons.git &> /dev/null
 git config core.sparseCheckout true
 echo "/optimized/" >> .git/info/sparse-checkout
 git fetch --tags
-latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+latestVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-# Check out latest release
-echo "Checking out ${latestTag}"
+# Check if already on latestVersion
+if [[ "${currentVersion}" == "${latestVersion}"  ]]; then
+  echo "Already on latest version (${latestVersion}). To force a re-install, delete resources/heroicons".
+  exit
+fi
 
-git checkout -q "${latestTag}"
+# Install latestVersion
+
+if [ "${currentVersion}" == "0" ]; then
+  echo "Installing heroicon svg files ${latestVersion}"
+else
+  echo "Updating heroicon svg files from ${currentVersion} to ${latestVersion}"
+fi
+
+# Checkout latest version of tailwindlabs/heroicons
+git checkout -q "${latestVersion}"
+
+# Reset resources/heroicons
+if [ -d "${BASEDIR}/resources/heroicons" ]; then
+  rm -Rf "${BASEDIR}/resources/heroicons"
+fi
+mkdir -p "${BASEDIR}/resources/heroicons"
+
+# Move svg files to resources
+mv "${BASEDIR}/.tmp/optimized/"* "${BASEDIR}/resources/heroicons/"
 
 # Note version in .version
-echo "${latestTag}" > "${BASEDIR}/resources/heroicons/.version"
+echo "${latestVersion}" > "${BASEDIR}/resources/heroicons/.version"
 
-# Remove heroicons .git dir
-echo "Removing heroicons .git directory"
+# Remove .tmp
+rm -Rf "${BASEDIR}/.tmp/"
 
-rm -Rf .git
-
-echo "Updated heroicon svg files to latest version"
+echo "Done!"
